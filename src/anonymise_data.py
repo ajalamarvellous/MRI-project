d51a4a4c5d82433c4b4fcf40c0e539a052e00ed6
shelby <ajalaoluwamayowa00@gmail.com>
2024-05-10 21:58:40 +0100

    Rename and anonymise user info on filename

---
+++ src/anonymise_data.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+from pathlib import Path
+
+import yaml
+
+DATA = Path("../data")
+
+def load_yaml(folder: str | Path) -> dict:
+    f = open(folder, "r")
+    maps = yaml.safe_load(f)
+    f.close()
+    return maps
+
+def anonymize(maps: dict, folder: str, home: str=DATA) -> None:
+    previous_address = home / folder
+    new_address = home / maps[folder]
+    shutil.move(previous_address, new_address)
+    print(f"Data previously in folder {folder[:3]} now loacated in {new_address}")
+
+def main():
+    print(f"Current location: {Path(__file__)}")
+    maps = load_yaml(DATA / "anonymise.yaml")
+    for folder in os.listdir(DATA.__str__()):
+        if not (folder.endswith("yaml") or folder == ".DS_Store"):
+            anonymize(maps, folder)
+
+if __name__ == "__main__":
+    main()
