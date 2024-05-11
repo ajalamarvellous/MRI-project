b49c77568433200ad998b37ab758a158794b6653
shelby <ajalaoluwamayowa00@gmail.com>
2024-05-11 13:21:08 +0100

    Add get_csv to get each image's address and label

---
+++ src/prepare_dataset.py
@@ -0,0 +1,43 @@
+import os
+import csv
+
+pos_tags = ["DHFDLAFH", "FJKAHFBC", "DHAJFVIF", "SHDCBDJEL"]
+
+def get_csv(dir : str, file_loc : str, pos_tags : list=pos_tags) -> None:
+    """
+    Create a CSV file that matches the file address with the tags
+    folders in pos_tags are positive examples of what we are test for while
+    others are negative.
+
+    Parameter(s)
+    ----------------
+    dir : str
+        Home directory where the dataset is
+    file_loc : str 
+        Location to save the file
+    pos_tags : list
+        List of folders containing pos examples
+
+    Return(s)
+    -------------
+    None
+
+    """
+    with open(file_loc, "w") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(["location", "label"])
+        for dir_, folder, files in os.walk(dir):
+            # check if it's postive image folder
+            if dir_.split("/")[-1] in pos_tags:
+                # save to file if it's a dcm image with the positive tag
+                rows = [(f"{dir_}/{file}", 1) for file in files if file.endswith(".dcm")]
+                csv_writer.writerows(rows) 
+            # Other control examples
+            else:
+                # save to file with negative tag if it's also a dcm image
+                rows = [(f"{dir_}/{file}", 0) for file in files if file.endswith(".dcm")]
+                csv_writer.writerows(rows) 
+
+if __name__ == "__main__":
+    data_dir = "../data"
+    file_dir = "../data/dataset.csv"
