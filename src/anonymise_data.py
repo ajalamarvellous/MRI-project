528033c482ea405de115613f3d4cf2c417e5e486
shelby <ajalaoluwamayowa00@gmail.com>
2024-05-14 19:17:59 +0100

    Update parameters

--- src/anonymise_data.py
+++ src/anonymise_data.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 import yaml
 
-DATA = Path("../data")
+DATA = Path("../data/images")
+
 
 def load_yaml(folder: str | Path) -> dict:
     f = open(folder, "r")
@@ -12,18 +13,21 @@ def load_yaml(folder: str | Path) -> dict:
     f.close()
     return maps
 
-def anonymize(maps: dict, folder: str, home: str=DATA) -> None:
+
+def anonymize(maps: dict, folder: str, home: Path = DATA) -> None:
     previous_address = home / folder
     new_address = home / maps[folder]
     shutil.move(previous_address, new_address)
-    print(f"Data previously in folder {folder[:3]} now loacated in {new_address}")
+    print(f"Data previously in folder {folder[:3]} now located in {new_address}")
+
 
 def main():
     print(f"Current location: {Path(__file__)}")
-    maps = load_yaml(DATA / "anonymise.yaml")
+    maps = load_yaml(DATA.parent / "anonymise.yaml")
     for folder in os.listdir(DATA.__str__()):
         if not (folder.endswith("yaml") or folder == ".DS_Store"):
             anonymize(maps, folder)
+
 
 if __name__ == "__main__":
     main()
