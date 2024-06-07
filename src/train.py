2b14371cb5ec10231958480f5f89552953086176
shelby <ajalaoluwamayowa00@gmail.com>
2024-06-07 10:41:07 +0100

    Refactor train function

--- src/train.py
+++ src/train.py
@@ -2,31 +2,41 @@ import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 
+import mlflow
 from models import ConvNet
 from prepare_dataset import DicomDataset, stratify_split
 from trainer import Trainer
-from utils import return_ylabels, seedall
+from utils import load_model, return_ylabels, seedall
 
 # PARAMETERS
 seedall(42)
-lr = 0.001
-epochs = 10
-batch_size = 16
+lr = 0.01
+epochs = 100
+batch_size = 4
 num_workers = 0
 num_classes = 1
 val_size = 0.3
 test_size = 0.2
 device = torch.device("cpu")
-FILE_PATH = "../data/temp_data.csv"
+fresh_training = True
+last_stop = 10
+MODEL_ARTIFACT = "../models/batch_4/ConvNet_9.pth"
+FILE_PATH = "../data/training.csv"
+MLFLOW_TRACKING_URI = "../mlflow/"
 
+# setting mlflow parameters
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+mlflow.set_experiment("MRI Hearing ache project")
 # getting dataset
 dataset = DicomDataset(FILE_PATH)
 y_labels = return_ylabels(FILE_PATH)
 # Get samplers
-splits = stratify_split(y_labels, val_size=val_size, test_size=test_size)
-train_sampler = SubsetRandomSampler(indices=splits[2])
-test_sampler = SubsetRandomSampler(indices=splits[1])
-val_sampler = SubsetRandomSampler(indices=splits[0])
+splits, class_weight = stratify_split(y_labels, test_size=test_size)
+train_sampler = SubsetRandomSampler(indices=splits[1])
+test_sampler = SubsetRandomSampler(indices=splits[0])
+
+# for batch_size in batch_size_:
+mlflow.log_param("Eval param", "bEST")
 # Get dataloaders
 train_loader = DataLoader(
     dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers
@@ -34,15 +44,21 @@ train_loader = DataLoader(
 test_loader = DataLoader(
     dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers
 )
-val_loader = DataLoader(
-    dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers
-)
+
+
 # Defining Model
 model = ConvNet(num_classes)
 model = model.to(device)
-# Specifying optimizer and criterion
 optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-criterion = torch.nn.BCELoss()
+
+if not fresh_training:
+    checkpoint = load_model(MODEL_ARTIFACT)
+    epochs = epochs - last_stop
+    model.load_state_dict(checkpoint["model_checkpoint"])
+    optimizer.load_state_dict(checkpoint["optimiser_checkpoint"])
+
+# Specifying optimizer and criterion
+criterion = torch.nn.BCELoss(weight=torch.Tensor([class_weight]))
 trainer = Trainer(
     epochs=epochs,
     lr=lr,
@@ -50,6 +66,9 @@ trainer = Trainer(
     criterion=criterion,
     optimiser=optimizer,
     device=device,
+    track_mlflow=True,
+    model_dir="../models/new_1",
+    result_dir="../results/new_1",
 )
 # training
-trainer.train(train_loader, test_loader)
+trainer.train(train_loader, test_loader, eval_every=1)
